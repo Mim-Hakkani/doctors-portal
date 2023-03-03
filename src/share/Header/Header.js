e77ef37b44451a8604103a202606bc6231517326
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-03-03 15:19:01 +0600

    add the images in doctors portal

--- src/share/Header/Header.js
+++ src/share/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
 <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a href="# "className="btn btn-ghost normal-case text-xl" >daisyUI</a>
+    <Link to="/" className="btn btn-ghost normal-case text-xl" >Doctors Portal</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
